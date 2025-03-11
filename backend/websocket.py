3cf98f59145f077c18d70adb2acf49afed6e25ca
Amir Hossein Pouria <amirporia99.1378@gmail.com>
2025-03-11 13:07:10 +0330

    remove prints

--- backend/websocket.py
+++ backend/websocket.py
@@ -80,10 +80,6 @@ async def handle_websocket(websocket: WebSocket):
                         
                         # Confirmed the transcribe by reviewing two times
                         transcribe, confirmed_transciption, confirm_offset_time = confirmation_process(transcribe, tokenize_transcription, confirmed_transciption, confirm_offset_time)
-
-                        print(f"%%%%%%%%%%%%%%%%%%% {" ".join([w for a,b,w in confirmed_transciption])}")
-                        print(f"%%%%%%%%%%%%%%%%%%% {confirmed_transciption}")
-                        print(f"^^^^^^^^^^^^^^^^^^^ {confirm_offset_time}")
   
                         if int(len(pcm_buffer)/(SAMPLE_RATE * BYTES_PER_SAMPLE)) >= 30 and len(tokenize_transcription) > 0:
                             # Trimming buffer when reach to 30s
@@ -97,9 +93,6 @@ async def handle_websocket(websocket: WebSocket):
                             elif pcm_buffer_idx != 0:
                                 pcm_buffer = bytearray(pcm_buffer[pcm_buffer_idx:])
         
-                        print(f"^^^^^^^^^^^^^^^^^^^ {" ".join([w for a,b,w in confirmed_transciption])}")
-                        print(f"^^^^^^^^^^^^^^^^^^^ {confirmed_transciption}")
-                        print(f"******************* {confirm_offset_time}")
                         response = {"lines": [{"speaker": "0", "text": " ".join(item[2] for item in confirmed_transciption)}]}
                         await websocket.send_json(response)
 
